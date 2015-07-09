e9e99ec73f404517013c50fb9498183cfe8161f6
Scott Williams <scott@swilliams.me>
2015-07-09 12:32:17 -0700

    Add project and icons

---
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Scott Williams on 7/9/15.
+//  Copyright (c) 2015 Scott Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
