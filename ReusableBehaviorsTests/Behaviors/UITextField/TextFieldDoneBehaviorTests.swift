d463216dd014f47e4d2a7fde3ea0dc80f6f8cd70
Scott Williams <scott@swilliams.me>
2015-07-11 22:06:44 -0700

    Add tests for textfield container

--- ReusableBehaviorsTests/Behaviors/UITextField/TextFieldDoneBehaviorTests.swift
+++ ReusableBehaviorsTests/Behaviors/UITextField/TextFieldDoneBehaviorTests.swift
@@ -14,16 +14,6 @@ class TextFieldDoneBehaviorTest: XCTestCase {
     let behavior = TextFieldDoneBehavior()
     let tf = UITextField(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testBehaviorResignsFirstResponder() {
         tf.becomeFirstResponder()
 //        XCTAssertTrue(tf.isFirstResponder(), "Expect becomeFirstResponder to be true")
--- ReusableBehaviorsTests/Behaviors/UITextField/TextFieldMaxLengthBehaviorTests.swift
+++ ReusableBehaviorsTests/Behaviors/UITextField/TextFieldMaxLengthBehaviorTests.swift
@@ -14,16 +14,6 @@ class TextFieldMaxLengthBehaviorTests: XCTestCase {
     let behavior = TextFieldMaxLengthBehavior()
     let tf = UITextField()
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testMaxLength() {
         behavior.maxLength = 5
         let range = NSMakeRange(0, 1)
