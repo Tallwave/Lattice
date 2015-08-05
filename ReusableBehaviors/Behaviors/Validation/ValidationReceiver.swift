30505fe52aa3390ee4f83dbec24c90f726d53b40
Scott Williams <scott@swilliams.me>
2015-08-05 11:57:00 -0700

    Add failure action for validation

--- ReusableBehaviors/Behaviors/Validation/ValidationReceiver.swift
+++ ReusableBehaviors/Behaviors/Validation/ValidationReceiver.swift
@@ -11,17 +11,18 @@ import UIKit
 public protocol ValidationReceiver {
     func validationUpdated(sender: ValidatorContainer)
     var successAction: ValidationAction? { get set }
-    
+    var failureAction: ValidationAction? { get set }
 }
 
-public class ValidationReceiverTransition: Behavior, ValidationReceiver {
+public class PostValidationTransitioner: Behavior, ValidationReceiver {
     @IBOutlet public weak var successAction: ValidationAction?
+    @IBOutlet public weak var failureAction: ValidationAction?
     
     @IBAction public func validationUpdated(sender: ValidatorContainer) {
         if sender.isValid {
             success()
         } else {
-            failure()
+            failure(sender.failingValidators)
         }
     }
     
@@ -31,8 +32,10 @@ public class ValidationReceiverTransition: Behavior, ValidationReceiver {
         }
     }
     
-    private func failure() {
-        println("FAILURE")
+    private func failure(failingValidators: [Validator]) {
+        if let handler = failureAction {
+            handler.performAction(forValidators: failingValidators)
+        }
     }
 }
 
@@ -49,5 +52,35 @@ public class SegueTransitioner: ValidationAction {
         if let segueIdentifier = self.segueIdentifier, controller = self.controller {
             controller.performSegueWithIdentifier(segueIdentifier, sender: nil)
         }
+    }
+}
+
+public class BaseValidationFailureTransitioner: ValidationAction {
+    public override func performAction(forValidators validators: [Validator]?) {
+        if let failingValidators = validators {
+            displayFailures(failingValidators)
+        }
+    }
+    
+    func displayFailures(failingValidators: [Validator]) {
+        for validator in failingValidators {
+            displayFailure(validator)
+        }
+    }
+    
+    func displayFailure(validator: Validator) {
+    }
+}
+
+public class AlertValidationFailureTransitioner: BaseValidationFailureTransitioner {
+    @IBOutlet weak var controller: UIViewController!
+    
+    override func displayFailures(failingValidators: [Validator]) {
+        let errorMessage = join("\n", failingValidators.map({ $0.errorMessage }))
+        let alert = UIAlertController(title: "Validation Errors", message: errorMessage, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        controller.presentViewController(alert, animated: true, completion: nil)
     }
 }
