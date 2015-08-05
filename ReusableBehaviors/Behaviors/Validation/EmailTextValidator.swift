2013feddf1a167ea0409b3e61771094e24cfc14f
Scott Williams <scott@swilliams.me>
2015-08-05 13:23:58 -0700

    Docs

--- ReusableBehaviors/Behaviors/Validation/EmailTextValidator.swift
+++ ReusableBehaviors/Behaviors/Validation/EmailTextValidator.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
+/**
+Checks if a string is a valid email. The pattern it matches against is 
+intentionally naive. The [official email regex](http://www.ex-parrot.com/pdw/Mail-RFC822-Address.html) 
+is nearly as long as freaking Beowulf, and since emails are squirrelly little 
+things, has a lot of false negatives. If you really want to do that knock 
+yourself out.
+*/
 public class EmailTextValidator: RegularExpressionValidator {
     private let _pattern = "^\\S+@\\S+\\.\\S+$"
+    
+    /**
+    The override removes IBInspectable, though since it's on the base class, it 
+    will still show up in Interface Builder.
+    */
     override var pattern: String {
         get { return _pattern }
         set {}
