da7bb03cd767feec39b81cd4d089638b9ea536d5
Scott Williams <scott@swilliams.me>
2015-08-05 12:50:35 -0700

    Add min/max validators

---
+++ ReusableBehaviors/Behaviors/Validation/MaxLengthValidator.swift
@@ -0,0 +1,17 @@
+//
+//  MaxLengthValidator.swift
+//  ReusableBehaviors
+//
+//  Created by Scott Williams on 8/5/15.
+//  Copyright (c) 2015 Tallwave. All rights reserved.
+//
+
+import UIKit
+
+public class MaxLengthValidator: TextControlValidator {
+    @IBInspectable var maximumLength: Int = 0
+    
+    override func validateText(text: String) -> Bool {
+        return count(text) <= maximumLength
+    }
+}
