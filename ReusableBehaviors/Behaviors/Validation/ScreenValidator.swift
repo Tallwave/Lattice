ada4cd5a88b0278648d5b23a0b154eb4adf979a0
Scott Williams <scott@swilliams.me>
2015-08-05 08:42:37 -0700

    Create ScreenValidator

--- ReusableBehaviors/Behaviors/Validation/ScreenValidator.swift
+++ ReusableBehaviors/Behaviors/Validation/ScreenValidator.swift
@@ -8,6 +8,16 @@
 
 import UIKit
 
-class ScreenValidator: NSObject {
-   
+public class ScreenValidator: UIControl {
+    @IBOutlet public var validators: [Validator]?
+
+    @IBAction public func validate(sender: AnyObject) {
+        if let invalidResults = validators?.filter({ !$0.validate() }) {
+            sendInvalidResults(invalidResults)
+        }
+    }
+    
+    private func sendInvalidResults(invalidResults: [Validator]) {
+        sendActionsForControlEvents(.ValueChanged)
+    }
 }
