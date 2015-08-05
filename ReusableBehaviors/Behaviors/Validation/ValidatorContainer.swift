bf957ecde0dcbf2470144cc41d70f8d65daed8b4
Scott Williams <scott@swilliams.me>
2015-08-05 10:55:44 -0700

    Rename ScreenValidator -> ValidationContainer

--- ReusableBehaviors/Behaviors/Validation/ValidatorContainer.swift
+++ ReusableBehaviors/Behaviors/Validation/ValidatorContainer.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-public class ScreenValidator: BehaviorControl {
+
+// I would love to be able to refactor a protocol out of this, but the argument that is passed along with the `ValueChanged` event must be a class, so there's not really a point.
+public class ValidatorContainer: BehaviorControl {
     @IBOutlet public var validators: [Validator]?
     
     public private(set) var failingValidators: [Validator] = []
+    
+    public var isValid: Bool {
+        return failingValidators.count == 0
+    }
 
     @IBAction public func validate(sender: AnyObject) {
         if let invalidResults = validators?.filter({ !$0.validate() }) {
