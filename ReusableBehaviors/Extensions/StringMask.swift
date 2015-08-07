ef7b30c54b4f328b8f9b52300376ad72cfc621b2
Scott Williams <scott@swilliams.me>
2015-08-07 13:29:30 -0700

    Adapts method

--- ReusableBehaviors/Extensions/StringMask.swift
+++ ReusableBehaviors/Extensions/StringMask.swift
@@ -6,7 +6,12 @@
 //  Copyright (c) 2015 Tallwave. All rights reserved.
 //
 
+// NSMatchingOptions.WithoutAnchoringBounds 1 << 4
+
 import UIKit
+
+private let resultIndex = 0
+private let lengthIndex = 0
 
 struct StringMask {
     var regex: NSRegularExpression
@@ -57,11 +62,93 @@ struct StringMask {
         let validChars = validCharactersFor(string)
         let pattern = NSMutableString(string: regex.pattern)
         let formattedString = NSMutableString()
+        let range = NSRange(location: 0, length: count(validChars))
+        let formattedRange = NSRange(location: 0, length: formattedString.length)
+        let newPattern = patternStep(pattern, onString: validChars, iteration: 1, resultFetcher: formattedString, range: range, placeholder: placeholder)
+        formattedString.replaceOccurrencesOfString(newPattern,
+            withString: pattern as String,
+            options: NSStringCompareOptions.RegularExpressionSearch,
+            range: formattedRange)
         return formattedString as String
     }
 
     func validCharactersFor(string: String) -> String {
-        return ""
+        let pattern = NSMutableString(string: regex.pattern)
+        var error: NSError?
+        let firstGroupPattern = getStepPattern(pattern, iteration: 1)
+        let validCharacters = NSMutableString()
+        for var i = 2; firstGroupPattern != nil; i++ {
+            let n: UInt = 0
+            var adaptingResult = []
+            var result: NSTextCheckingResult?
+
+            // gonna break
+            while result?.range.length != adaptingResult[lengthIndex].integerValue {
+                // add a capturing group to the pattern
+                adaptingResult = adaptsFirstGroupPattern
+            }
+        }
+//        for (int i = 2; firstGroupPattern != nil; i++)
+//        {
+//            do
+//            {
+//                // Add a capturing group to the pattern
+//                adaptingResult = [self adaptsFirstGroupPattern:firstGroupPattern subtracting:n];
+//                if (adaptingResult[kLENGTH_INDEX] == 0) break;
+//
+//                firstGroupPattern = adaptingResult[kRESULT_INDEX];
+//
+//                // Try to match the pattern
+//                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:firstGroupPattern
+//                    options:NSRegularExpressionCaseInsensitive
+//                    error:&error];
+//
+//                result = [regex firstMatchInString:string options:NSMatchingWithoutAnchoringBounds range:NSMakeRange(0, string.length)];
+//                if (! result) break;
+//
+//                NSString *matchedString = [string substringWithRange:result.range];
+//
+//                string = [string stringByReplacingCharactersInRange:result.range withString:@""];
+//
+//                [validCharacters appendString:matchedString];
+//
+//                n += result.range.length;
+//            }
+//                while (result.range.length != [adaptingResult[kLENGTH_INDEX] integerValue]);
+//
+//            firstGroupPattern = [self getStepPattern:&pattern iter:i];
+//        }
+//        
+//        return validCharacters;
+    }
+
+    private func adaptsFirstGroupPattern(group: String, subtracting n: UInt) -> (String, Int) {
+        var error: NSError?
+        // Gets the expected maximum repetition for the current group
+        let maxRepetEx = NSRegularExpression(pattern: "\\{((\\d+)?(?:,(\\d+)?)?)\\}",
+            options: .AnchorsMatchLines,
+            error: &error)!
+        let groupRange = NSRange(location: 0, length: count(group))
+        let numRep = maxRepetEx.firstMatchInString(group,
+            options: .WithoutAnchoringBounds,
+            range: groupRange)!
+
+        // Tries to get the maximum
+        var range = numRep.rangeAtIndex(3)
+        if range.location == NSNotFound {
+            range = numRep.rangeAtIndex(2)
+        }
+        var numberOfReps = (group as NSString).substringWithRange(range).toInt()!
+
+        // Replaces the the content of braces with {1, numberOfRepetitions}
+        if numberOfReps > 0 {
+            let val = numberOfReps - Int(n)
+            let format = NSString(format: "1,%lu", val)
+            let newGroup = (group as NSString).stringByReplacingCharactersInRange(numRep.rangeAtIndex(1), withString: format as String)
+        } else {
+            numberOfReps = NSIntegerMax
+        }
+        return (group, numberOfReps)
     }
 
     private func patternStep(pattern: NSMutableString, onString string: String, iteration: Int64, resultFetcher mutableResult: NSMutableString, range: NSRange, placeholder: String) -> String {
