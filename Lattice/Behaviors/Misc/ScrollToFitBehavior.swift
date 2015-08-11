81f4203cc19cb9017af243d0111541d64ce000fa
Scott Williams <scott@swilliams.me>
2015-08-11 08:13:15 -0700

    Docs

--- Lattice/Behaviors/Misc/ScrollToFitBehavior.swift
+++ Lattice/Behaviors/Misc/ScrollToFitBehavior.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 /**
-    When the onscreen keyboard overlaps a control, slide the screen up to bring the control back into frame.
+    When the onscreen keyboard overlaps a control, slide the screen up to bring the control back into view.
     
-    This works by modifying the top most constraint (defined here).
+    This works by modifying the top most constraint of the scene.
+
+## Usage
+1. Drag an Object onto the scene in Interface Builder
+2. Set its class to `ScrollToFitBehavior`
+3. Connect the `owner`, `constraintToModify`, and `containingView` outlets
+4. Connect the `textControls` outlet array to all the text controls (`UITextField` or `UITextView`) you want `ScrollToFitBehavior` to watch
+
 */
 public class ScrollToFitBehavior: Behavior {
     private var originalConstraintValue: CGFloat?
 
+    /**
+        The constraint that will be modified to bring the text control back into view.
+    */
     @IBOutlet public weak var constraintToModify: NSLayoutConstraint?
+    /**
+        Which view to send `setNeedsUpdateContraints` to.
+    */
     @IBOutlet public weak var containingView: UIView?
     /**
+        A list of text controls to watch. When any of them becomes firstResponder, this Behavior will check to see if the keyboard is obscuring it.
+    
+        Notes:
         `UIView` is the first common ancestor for both `UITextField` and `UITextView`. When the keyboard is shown (via `UIKeyboardWillShowNotification`) the class will scan this array to see if any of its views are `firstResponder`, than act accordingly. 
     
         Considering a `willSet` accessor to screen for just text views, but there may be other cases where allowing a generic `UIView` in is required.
